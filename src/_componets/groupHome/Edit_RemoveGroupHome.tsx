df5c5fb8a4dd6d83737f55d1cbec332edf7e012e
alain descartes <ualaindescartes@gmail.com>
2025-04-23 22:28:35 -0600

    added delete functionality for groupHomes

--- src/_componets/groupHome/Edit_RemoveGroupHome.tsx
+++ src/_componets/groupHome/Edit_RemoveGroupHome.tsx
@@ -3,6 +3,19 @@ import { useEffect, useState } from 'react';
 import { GroupHomeFetch } from '@/interfaces/groupHomeInterface';
 import useAuth from '@/lib/hooks/useAuth';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 
 export default function Edit_RemoveGroupHome() {
   const [homes, setHomes] = useState<GroupHomeFetch[]>([]);
@@ -38,8 +51,36 @@ export default function Edit_RemoveGroupHome() {
   useEffect(() => {
     getAllHomes();
   }, []);
+
+  const deleteHome = async (id: string) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/grouphome-route/delete-groupHome/${id}`,
+        {
+          method: 'POST',
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+      // const data = await response.json();
+      if (response.ok) {
+        setHomes(prev => prev.filter(item => item.id !== parseInt(id)));
+        toast('GroupHome deleted successfully.', {
+          style: { backgroundColor: 'green', color: 'white' },
+        });
+      }
+    } catch (error: any) {
+      toast('Error deleting group home', {
+        style: { backgroundColor: 'red', color: 'white' },
+      });
+      console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <div className="p-6">
+    <div className="p-4 sm:p-6 space-y-4">
       {isLoading &&
         Array.from({ length: 5 }).map((_, index) => (
           <div
@@ -62,9 +103,9 @@ export default function Edit_RemoveGroupHome() {
         homes.map(home => (
           <div
             key={home.id}
-            className="flex items-center justify-between border rounded-lg p-4 mb-4 shadow-sm"
+            className="flex flex-col sm:flex-row sm:items-center justify-between border rounded-lg p-4 shadow-sm gap-4"
           >
-            <div className="flex items-center gap-4">
+            <div className="flex flex-col sm:flex-row items-center gap-4 flex-1">
               {home.image_url ? (
                 <img
                   src={home.image_url}
@@ -80,13 +121,39 @@ export default function Edit_RemoveGroupHome() {
                 <h3 className="text-xl font-semibold">{home.name}</h3>
               </div>
             </div>
-            <div className="flex gap-4">
-              <Link href={'#'}>
-                <span className="text-blue-600 hover:underline">Edit</span>
+            <div className="flex flex-row justify-center gap-4 sm:gap-4 sm:justify-end items-center">
+              <Link href={'#'} className="text-blue-600 hover:underline">
+                Edit
               </Link>
-              <Link href={'#'}>
-                <span className="text-red-600 hover:underline">Delete</span>
-              </Link>
+
+              <AlertDialog>
+                <AlertDialogTrigger asChild={true}>
+                  <Button
+                    disabled={isLoading}
+                    className="text-red-600 hover:underline p-0 h-auto bg-transparent hover:bg-transparent cursor-pointer"
+                  >
+                    {isLoading ? 'Deleting' : 'Delete'}
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete the home and remove
+                      all its data from our servers.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => deleteHome(home.id.toString())}
+                      className="bg-red-600"
+                    >
+                      Continue
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </div>
           </div>
         ))}
